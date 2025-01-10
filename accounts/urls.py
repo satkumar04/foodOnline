8078e8b33e2994d13fb1cfcf5177e33e5a97a4e7
satheeeshkumarp <satkumar04@gmail.com>
2025-01-10 22:00:19 +0530

    completed vendor and custoner login

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,11 +7,12 @@ urlpatterns = [
     path('registerUser/', views.registerUser, name='registerUser'),
     path('registerVendor/', views.registerVendor, name='registerVendor'),
 
-    path('login/', views.registerUser, name='login'),
-    # path('logout/', views.logout, name='logout'),
-    # path('myAccount/', views.myAccount, name='myAccount'),
-    # path('custDashboard/', views.custDashboard, name='custDashboard'),
-    # path('vendorDashboard/', views.vendorDashboard, name='vendorDashboard'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('myAccount/', views.myAccount, name='myAccount'),
+    path('custDashboard/', views.custDashboard, name='custDashboard'),
+    path('vendorDashboard/', views.vendorDashboard, name='vendorDashboard'),
 
     # path('activate/<uidb64>/<token>/', views.activate, name='activate'),
 
--- accounts/views.py
+++ accounts/views.py
@@ -2,15 +2,34 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
 from accounts.models import User, UserProfile
+from accounts.utils import detectUser
 from vendor.forms import VenderForm
 from .forms import UserForm
-from django.contrib import messages
+from django.contrib import messages,auth
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.core.exceptions import PermissionDenied
 
-# Create your views here.
+# Restrict the vendor from accessing the customer page
+def check_role_vendor(user):
+    if user.role == 1:
+        return True
+    else:
+        raise PermissionDenied
+
+
+# Restrict the customer from accessing the vendor page
+def check_role_customer(user):
+    if user.role == 2:
+        return True
+    else:
+        raise PermissionDenied
+
+
 
 def registerUser(request):
-
-    if(request.method== 'POST'):
+    if request.user.is_authenticated:
+       return redirect('dashboard')
+    elif(request.method== 'POST'):
      form = UserForm(request.POST)
      if form.is_valid():
         # Create the user using the form
@@ -43,7 +62,10 @@ def registerVendor(request):
     # if request.user.is_authenticated:
     #     messages.warning(request, 'You are already logged in!')
     #     return redirect('registerVendor')
-   if request.method == 'POST':
+   if request.user.is_authenticated:
+       return redirect('dashboard')
+
+   elif request.method == 'POST':
         # store the data and create the user
         form = UserForm(request.POST)
         v_form = VenderForm(request.POST, request.FILES)
@@ -85,3 +107,77 @@ def registerVendor(request):
         }
 
    return render(request, 'accounts/registerVendor.html',context)
+
+
+def login(request):
+    if request.user.is_authenticated:
+       return redirect('myAccount')
+
+    elif(request.method == 'POST'):
+        email =  request.POST['email']
+        password = request.POST['password']
+        user = auth.authenticate(email = email,password = password)
+        if user is not None:
+            auth.login(request,user)
+            messages.success(request,'you have logged in successfuly')
+            return redirect('myAccount')
+        else:
+            messages.error(request,'Invalid login credential')    
+            return redirect('login')
+    return render(request, 'accounts/login.html')
+
+
+@login_required(login_url='login')
+def myAccount(request):
+    user = request.user
+    redirectUrl = detectUser(user)
+    return redirect(redirectUrl)
+@login_required(login_url='login')
+@user_passes_test(check_role_customer)
+def custDashboard(request):
+    # orders = Order.objects.filter(user=request.user, is_ordered=True)
+    # recent_orders = orders[:5]
+    # context = {
+    #     'orders': orders,
+    #     'orders_count': orders.count(),
+    #     'recent_orders': recent_orders,
+    # }
+    return render(request, 'accounts/custDashboard.html')
+
+@login_required(login_url='login')
+@user_passes_test(check_role_vendor)
+def vendorDashboard(request):
+    # vendor = Vendor.objects.get(user=request.user)
+    # orders = Order.objects.filter(vendors__in=[vendor.id], is_ordered=True).order_by('created_at')
+    # recent_orders = orders[:10]
+
+    # # current month's revenue
+    # current_month = datetime.datetime.now().month
+    # current_month_orders = orders.filter(vendors__in=[vendor.id], created_at__month=current_month)
+    # current_month_revenue = 0
+    # for i in current_month_orders:
+    #     current_month_revenue += i.get_total_by_vendor()['grand_total']
+    
+
+    # # total revenue
+    # total_revenue = 0
+    # for i in orders:
+    #     total_revenue += i.get_total_by_vendor()['grand_total']
+    # context = {
+    #     'orders': orders,
+    #     'orders_count': orders.count(),
+    #     'recent_orders': recent_orders,
+    #     'total_revenue': total_revenue,
+    #     'current_month_revenue': current_month_revenue,
+    # }
+    return render(request, 'accounts/vendorDashboard.html')
+
+
+
+def logout(request):
+    auth.logout(request)
+    messages.info(request,"you have logged out successfuly")
+    return redirect('login')
+
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
